3cc5336eccecc7c2ca6b6ca714b5b5ba96270d0b
Adio Adebayo <zillionaire234@gmail.com>
2023-04-09 07:37:11 +0100

    Update hut.py

--- hut.py
+++ hut.py
@@ -2,9 +2,9 @@
 """
 Fabric script that deletes out-of-date archives
 """
-from fabric.api import env, local, run
 from fabric.context_managers import lcd, cd
-from datetime import datetime
+from fabric.api import env, local, run
+
 
 env.hosts = ['54.85.99.227', '100.25.198.209']
 env.user = 'ubuntu'
